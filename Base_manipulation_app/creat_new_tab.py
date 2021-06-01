fd84eaf1d3236352e50dabea99e8043a8ba7ec10
lukas <lukasz.kordon@outlook.com>
2021-06-01 23:37:26 +0200

    add some error handling, add updating row function, add insert single record function, remove som minor display errors

--- Base_manipulation_app/creat_new_tab.py
+++ Base_manipulation_app/creat_new_tab.py
@@ -6,67 +6,86 @@ Created on Tue May  4 01:58:03 2021
 @author: lukas
 """
 from tabulate import tabulate
-
+import os
 
 class Create_Table:
     def list_all_database(self, mycursor):
-        mycursor.execute("SHOW DATABASES", multi=True)
-        print(tabulate(mycursor))
+        try:
+            mycursor.execute("SHOW DATABASES", multi=True)
+            print(tabulate(mycursor))
+        except:
+            print("Failed to list databasesdata!")  
   
     # create new empty table
     def create_table(self, mycursor):
-        sql = """
-        DROP TABLE IF EXISTS users;
-    
-        CREATE TABLE users (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            first_name VARCHAR(50) NOT NULL,
-            last_name VARCHAR(50) NOT NULL, 
-            age INT NOT NULL
-        );"""
-        mycursor.execute(sql, multi=True)
+        try:
+            sql = """
+            DROP TABLE IF EXISTS users;
+        
+            CREATE TABLE users (
+                id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                first_name VARCHAR(50) NOT NULL,
+                last_name VARCHAR(50) NOT NULL, 
+                age INT NOT NULL
+            );"""
+            mycursor.execute(sql, multi=True)
+        except:
+            print("Failed to create table!")  
       
         
     # insert to empty table some data
     def insert_data(self, mycursor, mydb):
-        sql = """
-        INSERT INTO users (first_name,last_name,age) 
-        VALUES ('Carine ','Schmitt',18),
-           ('Jean','King',24),
-           ('Jean','King',24),
-           ('Peter','Ferguson',65),
-           ('Janine ','Labrune',34),
-           ('Jonas ','Bergulfsen',73),
-           ('Janine ','Labrune',52),
-           ('Susan','Nelson',33),
-           ('Zbyszek ','Piestrzeniewicz',34),
-           ('Roland','Keitel',53),
-           ('Julie','Murphy',66),
-           ('Kwai','Lee',24),
-           ('Jean','King',63),
-           ('Susan','Nelson',45),
-           ('Roland','Keitel',33); """
-        mycursor.execute(sql, multi=True)
-        mydb.commit() 
+        try:
+            sql = """
+            INSERT INTO users (first_name,last_name,age) 
+            VALUES ('Carine ','Schmitt',18),
+               ('Jean','King',24),
+               ('Jean','King',24),
+               ('Peter','Ferguson',65),
+               ('Janine ','Labrune',34),
+               ('Jonas ','Bergulfsen',73),
+               ('Janine ','Labrune',52),
+               ('Susan','Nelson',33),
+               ('Zbyszek ','Piestrzeniewicz',34),
+               ('Roland','Keitel',53),
+               ('Julie','Murphy',66),
+               ('Kwai','Lee',24),
+               ('Jean','King',63),
+               ('Susan','Nelson',45),
+               ('Roland','Keitel',33); """
+            mycursor.execute(sql, multi=True)
+            mydb.commit()
+        except:
+            print("Failed to insert data!")  
 
       
            
-    # insert to table new data data
+    # insert to table new data data at the end of table
     def inse_singl_recor(self, mycursor, mydb, show_tabl_colum_data):
-        show_tabl_colum_data(mycursor)          
-        self.u = input("Insert id:\n")
-        self.x = input("Insert first_name:\n")
-        self.y = input("Insert last_name:\n")
-        self.z = input("Insert age:\n")
-        # inserting single record
-        self.sql = "INSERT INTO users (id, first_name, last_name, age) VALUES (%s, %s, %s, %s)"
-        self.val = (self.u, self.x, self.y, self.z)
-        mycursor.execute(self.sql, self.val, multi=True)
-        
-        print(mycursor.rowcount, "record added")
-        show_tabl_colum_data(mycursor)  
-        mydb.commit()
-        mydb.close()
+        try:
+            if show_tabl_colum_data(mycursor):                
+                self.x = input("Insert first_name:\n")
+                self.y = input("Insert last_name:\n")
+                
+                # check if age is int
+                self.age = input("Insert age:\n")
+                if  self.age.isnumeric():
+                     self.age = int(self.age)
+                else:
+                    print("You didn't enter a valid integer for age!")
+                    
+                # inserting single record
+                self.sql = "INSERT INTO users (first_name, last_name, age) VALUES (%s, %s, %s)"
+                self.val = (self.x, self.y, self.age)
+                mycursor.execute(self.sql, self.val, multi=True)
+                
+                print(mycursor.rowcount, "record added")
+                os.system('clear')
+                show_tabl_colum_data(mycursor)  
+                mydb.commit()
+                mydb.close()
+        except:
+            print("Failed to insert record!")  
     
      
 
--- Base_manipulation_app/main_app.py
+++ Base_manipulation_app/main_app.py
@@ -41,26 +41,29 @@ def show_tabl_colum_names(mycursor):
         # mydb.commit()
         return myresult
     except:
-        print("Failed to print data")    
+        print("Failed to print data!")    
 
 
 # show all items from tab
 def show_tabl_colum_data(*args):
-    # try:
-    len_args = len(args)
-    if len_args > 1:
-        args[0].execute(args[1], multi=True)      
-    else: 
-        args[0].execute("SELECT * FROM users", multi=True)
+    try:
+        len_args = len(args)
+        if len_args > 1:
+            args[0].execute(args[1], multi=True)      
+        else: 
+            args[0].execute("SELECT * FROM users", multi=True)
+            
+        myresult = args[0].fetchall() # checking all row   
+        # fetch only one row
+    
+        tab = show_tabl_colum_names(args[0])
+        a, b, c, d = tab
+        print(tabulate(myresult, headers=[c, b, d, a]))
         
-    myresult = args[0].fetchall() # checking all row   
-    # fetch only one row
-
-    tab = show_tabl_colum_names(args[0])
-    a, b, c, d = tab
-    print(tabulate(myresult, headers=[a, b, c, d]))
-    # except:
-    #     print("Failed to print data")  
+        return True
+    except:
+        print("Failed to print data!")
+        return False
         
         
 # count all duplicates in table and show them
@@ -80,129 +83,180 @@ def check_if_are_row_duplicates(mycursor):
             print(row)
         mycursor.close()
     except:
-        print("Failed to print data")
+        print("Failed to print data!")
     # mycursor.close()
    
     
 # finding and deliting duplicates
-def del_row_duplicats(mycursor): 
-    mydb = my_data_base()
-    sql = """    
-    delete t1 FROM users t1
-    INNER JOIN users t2
-    WHERE
-    t1.id < t2.id AND
-    t1.first_name = t2.first_name AND
-    t1.last_name = t2.last_name AND
-    t1.age = t2.age;
-    """
-    mycursor.execute(sql, multi=True)    
-    mydb.commit()
+def del_row_duplicats(mycursor, mydb):
+    try:
+        sql = """    
+        delete t1 FROM users t1
+        INNER JOIN users t2
+        WHERE
+        t1.id < t2.id AND
+        t1.first_name = t2.first_name AND
+        t1.last_name = t2.last_name AND
+        t1.age = t2.age;
+        """
+        mycursor.execute(sql, multi=True)    
+        mydb.commit()
+    except:
+        print("Can't delete duplicats!") 
+
     
     
 # sorting ascending or descending    
 def sort_funct(mycursor):
-    show_tabl_colum_data(mycursor)
-    options = ["Order by id [ascending]", "Order by first name [ascending]",
-               "Order by last name [ascending]", "Order by age[ascending]",
-               "Order by id [descending]", "Order by first name [descending]",
-               "Order by last name [descending]", "Order by age[descending]"]    
-     
-    choice = enquiries.choose('Choose one of these options: ', options)
-    # ascending
-    if choice == options[0]:
-        sql = "SELECT * FROM users ORDER BY id"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
-    elif choice == options[1]:
-        sql = "SELECT * FROM users ORDER BY first_name"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
-    elif choice == options[2]:
-        sql = "SELECT * FROM users ORDER BY last_name"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
-    elif choice == options[3]:
-        sql = "SELECT * FROM users ORDER BY age"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
-    # descending
-    elif choice == options[4]:
-        sql = "SELECT * FROM users ORDER BY id DESC"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
-    elif choice == options[5]:
-        sql = "SELECT * FROM users ORDER BY first_name DESC"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
-    elif choice == options[6]:
-        sql = "SELECT * FROM users ORDER BY last_name DESC"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
-    elif choice == options[7]:
-        sql = "SELECT * FROM users ORDER BY age DESC"
-        show_tabl_colum_data(mycursor, sql)
-        any_key_clear()
+    try: 
+        # if show is not false continue ele exept
+        if show_tabl_colum_data(mycursor):
+            show_tabl_colum_data(mycursor)
+            options = ["Order by id [ascending]", "Order by first name [ascending]",
+                       "Order by last name [ascending]", "Order by age[ascending]",
+                       "Order by id [descending]", "Order by first name [descending]",
+                       "Order by last name [descending]", "Order by age[descending]"]    
+             
+            choice = enquiries.choose('Choose one of these options: ', options)
+            # ascending
+            if choice == options[0]:
+                sql = "SELECT * FROM users ORDER BY id"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+            elif choice == options[1]:
+                sql = "SELECT * FROM users ORDER BY first_name"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+            elif choice == options[2]:
+                sql = "SELECT * FROM users ORDER BY last_name"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+            elif choice == options[3]:
+                sql = "SELECT * FROM users ORDER BY age"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+            # descending
+            elif choice == options[4]:
+                sql = "SELECT * FROM users ORDER BY id DESC"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+            elif choice == options[5]:
+                sql = "SELECT * FROM users ORDER BY first_name DESC"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+            elif choice == options[6]:
+                sql = "SELECT * FROM users ORDER BY last_name DESC"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+            elif choice == options[7]:
+                sql = "SELECT * FROM users ORDER BY age DESC"
+                show_tabl_colum_data(mycursor, sql)
+                any_key_clear()
+    except:
+        print("Failed to sort data!")  
         
 
 # deleting data support anty injection
-def delet_record(mycursor):
-    mydb = my_data_base()
-    show_tabl_colum_data(mycursor)  
-    
-    y = input("Select row id for delete.")
-    sql = "DELETE FROM users WHERE id= %s"
-    adr = (f"{y}", )
-    
-    mycursor.execute(sql, adr, multi=True)   
-    mydb.commit()
-    
-    print(mycursor.rowcount, "record deleted")
-    show_tabl_colum_data(mycursor)  
+def delet_record(mycursor, mydb):
+    try:
+        if show_tabl_colum_data(mycursor):
+            show_tabl_colum_data(mycursor)
+
+            y = input("Select row id for delete.")
+            sql = "DELETE FROM users WHERE id= %s"
+            adr = (f"{y}", )
+            
+            mycursor.execute(sql, adr, multi=True)   
+            mydb.commit()
+            
+            print(mycursor.rowcount, "record deleted")
+            show_tabl_colum_data(mycursor)
+    except:
+        print("Failed to delete record!")  
 
 
 # deleting whole table
 def del_table(mycursor):
-    sql = "DROP TABLE users"
-    mycursor.execute(sql, multi=True)
+    try:
+        sql = "DROP TABLE users"
+        mycursor.execute(sql, multi=True)
+    except:
+        print("Failed to delete table!")  
 
 
 # updating record in tab
-def updating_record(mycursor):
-    mydb = my_data_base()
-    show_tabl_colum_data(mycursor, mydb) 
+def updating_record(mycursor, mydb):
+    if show_tabl_colum_data(mycursor):
+        options = ["Uptdate: first_name.", "Uptdate: last_name.",
+           "Uptdate: age."]    
+             
+        choice = enquiries.choose('Choose one of these options: ', options)
+        # ascending
+        if choice == options[0]:
+            id_ = input("Insert column id:\n")
+            name = input("Insert new value to update:\n")
+        
+            # prepare query and data
+            query = """ UPDATE users
+                        SET first_name = %s
+                        WHERE id = %s """
+        elif choice == options[1]:
+            id_ = input("Insert column id:\n")
+            name = input("Insert new value to update:\n")
+        
+            # prepare query and data
+            query = """ UPDATE users
+                        SET last_name = %s
+                        WHERE id = %s """
+        elif choice == options[2]:
+            id_ = input("Insert column id:\n")
+            name = input("Insert new value to update:\n")
+        
+            # prepare query and data
+            query = """ UPDATE users
+                        SET age = %s
+                        WHERE id = %s """
+
+        data = (f"{name}", f"{id_}")
+
+        try:   
+            # update 
+            mycursor.execute(query, data)
     
-    x = input("Insert column name:\n")
-    y = input("Insert old value to update:\n")
-    z = input("Insert new value to update:\n")
+            # accept the changes
+            mydb.commit()
+            os.system('clear')
+            show_tabl_colum_data(mycursor)    
+        except:
+            print("Failed to updating record!")  
     
-    sql = f"UPDATE users SET {x} = %s WHERE id = %s"
-    val = (f"{y}", f"{z}")
-    
-    mycursor.execute(sql, val, multi=True) 
-    # required to make changes
-    mydb.commit()
-    
-    print(mycursor.rowcount, "record(s) affected")    
-    show_tabl_colum_data(mycursor)  
- 
+        finally:
+            mycursor.close()
+            mydb.close()
+            
     
 # limit tab elements for show
 def show_with_limits(mycursor):
-    mycursor.execute("SELECT * FROM users LIMIT 5", multi=True) 
-    myresult = mycursor.fetchall()
-    
-    for x in myresult:
-      print(x)     
+    try:
+        mycursor.execute("SELECT * FROM users LIMIT 5", multi=True) 
+        myresult = mycursor.fetchall()
+        
+        for x in myresult:
+          print(x)
+    except:
+        print("Failed to limit data!")  
 
 
 # limit range of displayed tabs data
 def show_with_range(mycursor):
-    mycursor.execute("SELECT * FROM users LIMIT 3 OFFSET 2", multi=True)
-    myresult = mycursor.fetchall()
-    
-    for x in myresult:
-      print(x)  
+    try:
+        mycursor.execute("SELECT * FROM users LIMIT 3 OFFSET 2", multi=True)
+        myresult = mycursor.fetchall()
+        
+        for x in myresult:
+          print(x)
+    except:
+        print("Failed to range data!")  
      
       
 # print menu      
@@ -218,7 +272,7 @@ def menu():
     "Delete record.",
     "Insert record.",
     "Deleting table.",
-    "Updating record in table.",
+    "Updating record.",
     "Limit table elements for show.",
     "Limit range of displayed tabs data.",
     "Close programm."]
@@ -247,7 +301,7 @@ def my_data_base():
         mydb = mysql.connector.connect(**conf)
         return mydb
     except:
-        print("Cant connect to database")
+        print("Cant connect to database!")
             
         
 #main function
@@ -282,7 +336,7 @@ def main():
             any_key_clear()
             
         elif choice == options[5]:          
-            del_row_duplicats(mycursor) 
+            del_row_duplicats(mycursor, mydb) 
             any_key_clear()
             
         elif choice == options[6]:
@@ -290,7 +344,7 @@ def main():
             any_key_clear()
             
         elif choice == options[7]:
-            delet_record(mycursor)
+            delet_record(mycursor, mydb)
             any_key_clear()
             
         elif choice == options[8]:
@@ -302,7 +356,7 @@ def main():
             any_key_clear()
             
         elif choice == options[10]:
-            updating_record(mycursor)
+            updating_record(mycursor, mydb)
             any_key_clear()
             
         elif choice == options[11]:
